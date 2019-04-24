709c6a03494caa11c380d495b8d915dda63e6aa0
ajthakare <thakare725@gmail.com>
2019-04-23 19:24:38 -0700

    changes for request update

--- client/src/front/views/farmerrequests/FarmerRequests.js
+++ client/src/front/views/farmerrequests/FarmerRequests.js
@@ -17,15 +17,15 @@ class FarmerRequests extends PureComponent {
     // const response = await fetch(`http://localhost:3002/api/myrequests/5cbd62b6a090d8249f70a016`);
     // const json = await response.json();
     // this.setState({ data: json });
-    console.log('Heyy');
+    //console.log('Heyy');
     let user_id = JSON.parse(localStorage.getItem('user_id'));
-    console.log('User_id' + user_id);
+    // console.log('User_id' + user_id);
     try {
       var url = 'http://localhost:3002/api/farmerrequests/';
       await fetch(url)
         .then(res => res.json())
         .then(json => {
-          console.log(json.message);
+          // console.log(json.message);
           var data = json.message; //gets data in string
           // console.log(typeof data);
           data = JSON.parse(data);
@@ -33,9 +33,33 @@ class FarmerRequests extends PureComponent {
           this.setState({ data: data });
         });
     } catch (error) {
-      console.log(error);
+      // console.log(error);
     }
   }
+
+  handleOnSubmit = (event: SyntheticEvent<>) => {
+    console.log('in handleonsubmit');
+    let reqid = document.getElementById('reqid').value;
+
+    let status = document.getElementById('newstatus').value;
+
+    fetch('http://localhost:3002/api/request/update', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        req_id: reqid,
+        status: status,
+      }),
+    })
+      .then(res => res.json())
+      .then(json => {
+        if (json.success) {
+          // console.log('update successfull');
+        }
+      });
+  };
 
   render() {
     let rows = this.state.data.map(request => {
@@ -95,7 +119,7 @@ class FarmerRequests extends PureComponent {
               <div className="form-group">
                 <label className="col-sm-2 control-label">Request ID:</label>
                 <div className="col-md-3">
-                  <input type="text" className="form-control" />
+                  <input type="text" id="reqid" className="form-control" />
                 </div>
               </div>
               <div className="form-group">
@@ -103,16 +127,20 @@ class FarmerRequests extends PureComponent {
                   Action:
                 </label>
                 <div className="col-md-2">
-                  <select className="form-control m-b-10">
-                    <option>Approve</option>
-                    <option>Decline</option>
-                    <option>Pending</option>
+                  <select id="newstatus" className="form-control m-b-10">
+                    <option value="Approved">Approve</option>
+                    <option value="Declined">Decline</option>
+                    <option value="Pending">Pending</option>
                   </select>
                 </div>
               </div>
               <div className="form-group">
                 <div className="col-lg-offset-2 col-lg-10">
-                  <button type="submit" className="btn btn-success">
+                  <button
+                    type="submit"
+                    onClick={this.handleOnSubmit}
+                    className="btn btn-success"
+                  >
                     Submit
                   </button>
                   <button type="reset" className="btn btn-danger">
