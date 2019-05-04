94718490e268a7fc5f1174e7e2f22aecda991b1c
Janhavi Dahihande <janhavidahihande@gmail.com>
2019-05-04 15:39:28 -0700

    Update index.js

--- client/src/front/components/tabPanel/index.js
+++ client/src/front/components/tabPanel/index.js
@@ -1,9 +1,9 @@
 // @flow
 
-export { default as TabPanel } from './tabPanel/TabPanel';
+export { default as TabPanel } from './TabPanel/TabPanel';
 export { default as TabPanelHeader } from './tabPanelHeader/TabPanelHeader';
 export { default as TabPanelBody } from './tabPanelBody/TabPanelBody';
 export {
   default as TabPanelBodyContent,
-} from './tabPanelBodyContent/TabPanelBodyContent';
+} from './TabPanelBodyContent/TabPanelBodyContent';
 export { default as TabPanelDemo } from './TabPanelDemo';
